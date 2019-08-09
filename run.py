2c704c10750a455c10dda3ed889f9695496a11b8
Andrew Campagna <40666746+andrewalexgit@users.noreply.github.com>
2019-08-09 13:13:51 -0400

    Update run.py

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import json
 from flask import Flask, render_template, request, redirect, jsonify
-import database as dbmodule
+from database import users_db
 import requests_toolbelt.adapters.appengine
 
 requests_toolbelt.adapters.appengine.monkeypatch()
